5b9b9df4adf710565989ebb3a6c95e632b54d582
Janson Leung <874361737@qq.com>
2015-07-01 10:45:39 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -3,17 +3,17 @@ require 'PHPExcelReader/PHPExcelReader.php';
 
 try{
 	$Reader = new PHPExcelReader('test.xls');
-	$total = $Reader->count();			// get the total rows of records
+	$total = $Reader->count();		// get the total rows of records
 	//$current = $Reader->current();	// get the current row data
 	
 	/*
-	$Reader->seek(4);					// skip to the 4th row 
-	$row = $Reader->current();			// get the 4th row data
+	$Reader->seek(4);			// skip to the 4th row 
+	$row = $Reader->current();		// get the 4th row data
 	*/
 	
 	/*
 	foreach($Reader as $key => $row){
-		$data[] = $row;					// loop obtain row data
+		$data[] = $row;			// loop obtain row data
 	}
 	*/
 	
